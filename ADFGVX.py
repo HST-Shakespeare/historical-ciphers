018828bdde291f9b7fdfe7ad264f00cc6ab32405
rapid-Man <marek.oszwaldowski@net-c.com>
2026-07-12 11:31:47 -0400

    Update ADFGVX.py

--- ADFGVX.py
+++ ADFGVX.py
@@ -1,1 +1,35 @@
+import intertools
+import random
 
+message = "hellothere"  #the message that is to be sent
+plaintext = tuple(message)
+#print(plaintext)
+
+boxLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+              "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+#the letters inside the ADFGVX square
+random.shuffle(boxLetters)
+boxValues = ["AA", "AD", "AF", "AG", "AV", "AX", "DA", "DD", "DF", "DG", "DV", "DX", "FA", "FD", "FF", "FG", "FV", "FX",
+             "GA", "GD", "GF", "GG", "GV", "GX", "VA", "VD", "VF", "VG", "VV", "VX", "XA", "XD", "XF", "XG", "XV", "XX"]
+#the square's coordinates
+boxKey = dict(zip(boxLetters, boxValues))
+#print(boxKey)
+diagraphList = []
+for l in plaintext:
+    diagraphList.append(boxKey[l])
+#print(diagraphList)
+substitutedText = list(itertools.chain.from_iterable(diagraphList))
+#print(substitutedText)
+
+z = []
+o = []
+3 = []
+def transpose(letter, start):
+  letter.append(substitutedText[start::3])
+transpose(z, 0)
+transpose(o, 1)
+transpose(e, 2)
+
+
+e = []
+#uncertain new stuff:
