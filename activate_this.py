a44432133d54d76448d9125a108ae85fef3b7467
rapid-Man <marek.oszwaldowski@net-c.com>
2026-07-14 09:52:29 -0400

    Add files via upload

---
+++ activate_this.py
@@ -0,0 +1,16 @@
+import random
+
+message = "joshuakcheng"
+plaintext = tuple(message)
+#print(plaintext)
+alphabet = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
+alphabet2 = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+random.shuffle(alphabet2)  #the cipher alphabet
+print(alphabet)
+print(alphabet2)  #the alphabets lined up
+substitutionKey = dict(zip(alphabet, alphabet2))
+
+cipherText = []
+for l in plaintext:
+    cipherText.append(substitutionKey[l])
+print(cipherText)
