dee90eca2e3cf0f7e34aa132a5dd779e73163de6
rapid-Man <marek.oszwaldowski@net-c.com>
2026-07-16 10:16:59 -0400

    Refactor Atbash cipher implementation

--- backend/atbashTest.py
+++ backend/atbashTest.py
@@ -1,20 +1,18 @@
 
 alphabet = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
 cipherAlphabet = ("z", "y", "x", "w", "v", "u", "t", "s", "r", "q", "p", "o", "n", "m", "l", "k", "j", "i", "h", "g", "f", "e", "d", "c", "b", "a")
-substitutionKey = dict(zip(alphabet, cipherAlphabet))
+key = dict(zip(alphabet, cipherAlphabet))
 print(alphabet)
 print(cipherAlphabet)
 
-message = "atbashcipher"  #generalize
-plaintext = tuple(message)
+def encrypt(message, key)
 
 ciphertext = []
-for l in plaintext:
-    ciphertext.append(substitutionKey[l])
+for l in message:
+    ciphertext.append(key[l])
 print(ciphertext)
 
-decryptionKey = dict(zip(cipherAlphabet, alphabet))
 decryptedText = []
-for k in ciphertext:
-    decryptedText.append(decryptionKey[k])
+for l in message:
+    decryptedText.append(key[l])
 print(decryptedText)
