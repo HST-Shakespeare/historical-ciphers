fbec3f0b2d0dccf6fc0ce7cf6b16b588ab8e1d0d
rapid-Man <marek.oszwaldowski@net-c.com>
2026-07-16 12:07:46 -0400

    Refactor ROT13 cipher to return values
    Refactor ROT13 cipher functions to return values instead of printing. Fix variable names and ensure proper error handling.

--- backend/ciphers/ROT13.py
+++ backend/ciphers/ROT13.py
@@ -1,24 +1,24 @@
 alphabet = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
 rotatedAlphabet = ("n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m")
 key = dict(zip(alphabet, rotatedAlphabet))
-print(alphabet)
-print(rotatedAlphabet)
+return(alphabet)
+return(rotatedAlphabet)
 
 def encrypt(message, key)
     try:
         ciphertext = []
         for l in message:
             ciphertext.append(key[l])
-            print(cipherText)
+            return(cipherText)
     except:
-        print("error. Please use only lowercase letters")
+        return("error. Please use only lowercase letters")
 
 def decrypt(message, key)
     try:
         decryptedText = []
         for k in ciphertext:
             decryptedText.append(key[k])
-        print(decryptedText)
+        return(decryptedText)
     except:
-        print("error. Please use only lowercase letters")
+        return("error. Please use only lowercase letters")
         
