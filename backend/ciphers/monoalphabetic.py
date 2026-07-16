d093b6aa31d82336d090bbcd597511e7db8746c9
rapid-Man <marek.oszwaldowski@net-c.com>
2026-07-16 12:15:03 -0400

    Refactor monoalphabetic cipher functions
    Refactor encryption and decryption logic into functions. Add error handling for invalid input.

--- backend/ciphers/monoalphabetic.py
+++ backend/ciphers/monoalphabetic.py
@@ -1,22 +1,28 @@
 import random
 
 message = "joshuakcheng"
-plaintext = tuple(message)
-#print(plaintext)
 alphabet = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
 alphabet2 = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 random.shuffle(alphabet2)  #the cipher alphabet
-print(alphabet)
-print(alphabet2)  #the alphabets lined up
+return(alphabet)
+return(alphabet2)  #the alphabets lined up
 substitutionKey = dict(zip(alphabet, alphabet2))
-
-cipherText = []
-for l in plaintext:
-    cipherText.append(substitutionKey[l])
-print(cipherText)
-
 decryptionKey = dict(zip(alphabet2, alphabet))
-decryptedText = []
-for k in cipherText:
-    decryptedText.append(decryptionKey[k])
-print(decryptedText)
+
+def encrypt(message)
+    try:       
+        cipherText = ""
+        for l in plaintext:
+            cipherText.append(substitutionKey[l])
+        return(cipherText)
+    except:
+        return("Error. Please use only lowercase letters")
+
+def decrypt():
+    try:
+        decryptedText = ""
+        for k in cipherText:
+            decryptedText.append(decryptionKey[k])
+        return(decryptedText)
+    except:
+        return:("error. please use only lowercase letters")
