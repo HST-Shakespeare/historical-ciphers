b9da2a4c471384071568ac86d117718851c43de4
rapid-Man <marek.oszwaldowski@net-c.com>
2026-07-16 09:11:19 -0400

    Refactor ROT13 implementation with error handling

--- backend/ROT13.py
+++ backend/ROT13.py
@@ -1,17 +1,20 @@
 alphabet = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
-cipherAlphabet = ("n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m")
-key = dict(zip(alphabet, cipherAlphabet))
+rotatedAlphabet = ("n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m")
+key = dict(zip(alphabet, rotatedAlphabet))
 print(alphabet)
-print(cipherAlphabet)
+print(rotatedAlphabet)
 
 def ROT13 (message):
-
-    ciphertext = []
-    for l in plaintext:
-        ciphertext.append(key[l])
-    print(ciphertext)
-
-    decryptedText = []
-    for k in ciphertext:
-        decryptedText.append(key[k])
-    print(decryptedText)
+    try:
+        ciphertext = []
+        for l in message:
+            ciphertext.append(key[l])
+        print(ciphertext)
+    
+        decryptedText = []
+        for k in ciphertext:
+            decryptedText.append(key[k])
+        print(decryptedText)
+    except:
+        print("error. Please use only lowercase letters")
+        
