4f402f070b4230c4d5455593447a99d83e8e95b4
rapid-Man <marek.oszwaldowski@net-c.com>
2026-07-16 09:00:26 -0400

    Refactor ROT13.py for correct variable handling
    Fix variable usage and improve ROT13 function.

--- backend/ROT13.py
+++ backend/ROT13.py
@@ -1,19 +1,17 @@
 alphabet = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
 cipherAlphabet = ("n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m")
-substitutionKey = dict(zip(alphabet, cipherAlphabet))
+key = dict(zip(alphabet, cipherAlphabet))
 print(alphabet)
 print(cipherAlphabet)
 
 def ROT13 (message):
-    plaintext = tuple(message)
 
     ciphertext = []
     for l in plaintext:
-        ciphertext.append(substitutionKey[l])
+        ciphertext.append(key[l])
     print(ciphertext)
 
     decryptedText = []
-    decryptionKey = dict(zip(cipherAlphabet, alphabet))
     for k in ciphertext:
-        decryptedText.append(decryptionKey[k])
+        decryptedText.append(key[k])
     print(decryptedText)
