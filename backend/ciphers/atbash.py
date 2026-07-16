9d4b3c2da247ef8d9e858bf7a83bb10f5ab0d438
rapid-Man <marek.oszwaldowski@net-c.com>
2026-07-16 11:31:44 -0400

    Refactor Atbash cipher to return values instead of print
    Refactor Atbash cipher implementation to return alphabets and ciphertext instead of printing. Adjust error handling for better clarity.

--- backend/ciphers/atbash.py
+++ backend/ciphers/atbash.py
@@ -2,22 +2,22 @@
 alphabet = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
 cipherAlphabet = ("z", "y", "x", "w", "v", "u", "t", "s", "r", "q", "p", "o", "n", "m", "l", "k", "j", "i", "h", "g", "f", "e", "d", "c", "b", "a")
 key = dict(zip(alphabet, cipherAlphabet))
-print(alphabet)
-print(cipherAlphabet)
+return(alphabet)
+return(cipherAlphabet)
 
 def encrypt(message, key):
     try:
-        ciphertext = []
+        ciphertext = ""
         for l in message:
             ciphertext.append(key[l])
-        print(ciphertext)
+        return(ciphertext)
     except:
-        print("error. Please use only lowercase letters")
+        return("error. Please use only lowercase letters")
 def decrypt(message, key):
     try:
-        decryptedText = []
+        decryptedText = ""
         for l in message:
             decryptedText.append(key[l])
-        print(decryptedText)
+        return(decryptedText)
     except:
-        print("error. please use only lowercase letters")
+        return("error. please use only lowercase letters")
