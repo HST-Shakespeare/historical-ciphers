f0ea28d5b57ff0da59651aed7417c91a0b8a1a93
3rdVoyager <palebluedot316@gmail.com>
2026-07-16 10:28:52 -0400

    testing FastAPI

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello from FastAPI!"}
+
+@app.get("/ciphers")
+def get_ciphers():
+    return {"ciphers": ["Caesar", "Vigenère", "Rail Fence"]}
+
+@app.get("/encrypt/{message}")
+def encrypt_message(message: str):
+    return message
