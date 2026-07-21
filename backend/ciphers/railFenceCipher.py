bd8c728f67a660dd7a116b58965628b5d9c7ce00
rapid-Man <marek.oszwaldowski@net-c.com>
2026-07-21 09:37:42 -0400

    Refactor encrypt function in railFenceCipher.py
    Refactor railFenceCipher to accept message and input as parameters, and return the ciphertext.

--- backend/ciphers/railFenceCipher.py
+++ backend/ciphers/railFenceCipher.py
@@ -1,13 +1,12 @@
 import itertools
-a = list(itertools.repeat([], 3))
 
-message = "guilford"
-plaintext = message.casefold()
-
-def encrypt(input):
+def encrypt(message, input):
+    plaintext = message.casefold()
+    a = list(itertools.repeat([], input))
     try:
-        for v in range(0, input)
-            a[v].append(message[v::input])
-        return(a[input-1])
+        for v in range(0, input):
+            a[v].append(plaintext[v::input]) #the elements in list a are actually changing in tandem so I have to print only the last one
+        ciphertext = "".join(a[len(a)-1])
+        return(ciphertext) #return the last element of list a
     except:
         return("error. input must be an integer")
