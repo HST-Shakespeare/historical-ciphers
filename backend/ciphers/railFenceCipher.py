af54fbb13ef00339419e30941356913ba9152ad8
rapid-Man <marek.oszwaldowski@net-c.com>
2026-07-22 10:08:34 -0400

    Implement decrypt function in railFenceCipher.py
    Added decrypt function for rail fence cipher.

--- backend/ciphers/railFenceCipher.py
+++ backend/ciphers/railFenceCipher.py
@@ -5,8 +5,19 @@ def encrypt(message, input):
     a = list(itertools.repeat([], input))
     try:
         for v in range(0, input):
-            a[v].append(plaintext[v::input]) #the elements in list a are actually changing in tandem so I have to print only the last one
+            a[v].append(plaintext[v::input]) #the elements in list a are actually changing in tandem
         ciphertext = "".join(a[len(a)-1])
         return(ciphertext) #return the last element of list a
     except:
         return("error. input must be an integer")
+
+def decrypt(message, input)
+    plaintext = message.casefold()
+    b = list(itertools.repeat([], input))
+    try:
+        for w in range(0, input)
+            b[w].append(plaintext[w::input])
+        ciphertext = "".join(b[len(b-1)])
+        return(ciphertext)
+    except:
+        return("error. please use only letters")
