df5c3d550337a531aa39d8c1ecb18bac21267f8c
rapid-Man <marek.oszwaldowski@net-c.com>
2026-07-23 09:24:36 -0400

    Fix function name and return in affineCipher.py
    Renamed 'incrypt' function to 'encrypt' and updated return statement.

--- backend/ciphers/affineCipher.py
+++ backend/ciphers/affineCipher.py
@@ -1,9 +1,9 @@
 alphabet = {"a":0, "b":1, "c":2, "d":3, "e":4, "f":5, "g":6, "h":7, "i":8, "j":9, "k":10, "l":11, "m":12, "n":13, "o":14, "p":15, "q":16, "r":17, "s":18, "t":19, "u":20, "v":21, "w":22, "x":23, "y":24, "z":25}
 d2 = {v: k for k, v in alphabet.items()} #alphabet swapped
 
-def incrypt(message, a, b):
+def encrypt(message, a, b):
     plaintext = message.casefold()
     for x in plaintext:
         eq = a * alphabet[x] + b
         result = eq % 26
-        print(d2[result])
+        return("".join(d2[result]), end="")
