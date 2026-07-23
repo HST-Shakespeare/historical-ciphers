d9337945c8f19ad005f701272712fa010d16c6e8
rapid-Man <marek.oszwaldowski@net-c.com>
2026-07-23 09:03:02 -0400

    Add files via upload

---
+++ backend/ciphers/affineCipher.py
@@ -0,0 +1,13 @@
+alphabet = {"a":0, "b":1, "c":2, "d":3, "e":4, "f":5, "g":6, "h":7, "i":8, "j":9, "k":10, "l":11, "m":12, "n":13, "o":14, "p":15, "q":16, "r":17, "s":18, "t":19, "u":20, "v":21, "w":22, "x":23, "y":24, "z":25}
+d2 = {v: k for k, v in alphabet.items()} #alphabet swapped
+message = "linear"
+plaintext = message.casefold()
+a = 5  # example value for 'a'
+b = 8  # example value for 'b'
+for x in plaintext:
+    eq = a * alphabet[x] + b
+    result = eq % 26
+    #print(result)
+    print(d2[result])
+
+# Print the key corresponding to the value 2
