62d1dbcec51984f33f4c486896c23fefa0511b78
rapid-Man <marek.oszwaldowski@net-c.com>
2026-07-23 11:42:58 -0400

    Refactor encrypt function to use ciphertext list
    The encrypt function now appends results to a ciphertext list and prints the final encrypted message. The code structure has been adjusted for better clarity and functionality.

--- backend/ciphers/affineCipher.py
+++ backend/ciphers/affineCipher.py
@@ -1,12 +1,13 @@
 alphabet = {"a":0, "b":1, "c":2, "d":3, "e":4, "f":5, "g":6, "h":7, "i":8, "j":9, "k":10, "l":11, "m":12, "n":13, "o":14, "p":15, "q":16, "r":17, "s":18, "t":19, "u":20, "v":21, "w":22, "x":23, "y":24, "z":25}
 d2 = {v: k for k, v in alphabet.items()} #alphabet swapped
-
+ciphertext = []
 def encrypt(message, a, b):
     try:
         plaintext = message.casefold()
         for x in plaintext:
             eq = a * alphabet[x] + b
             result = eq % 26
-        return("".join(d2[result]))
+            ciphertext.append(d2[result])
+        print("".join(d2[result]))
     except:
         return("error. please use only English letters")
